6d5319c9fe7707f290dade805f77a3b527d75d76
colderry <colderryz@gmail.com>
2021-11-29 20:07:49 +0600

    Ping Command

--- src/commands/core/General/Ping.js
+++ src/commands/core/General/Ping.js
@@ -1,24 +1,65 @@
-const { Message } = require("discord.js");
+const { Message, CommandInteraction } = require("discord.js");
 const Command = require("../../Command");
 
 class Ping extends Command {
-    constructor(client) {
+    constructor (client) {
         super(client, {
             name: "ping",
-            description: "",
-            aliases: [],
-            category: "general"
+            description: "The ping of the bot",
+            aliases: ["pong"],
+            category: "general",
+            cooldown: 10,
+            slash: {
+                name: "ping",
+                description: "The ping of the bot",
+                enableDM: true
+            }
         });
     };
 
     /**
-     * 
      * @param {Message} message 
      * @param {string[]} args 
+     * @returns 
      */
     run (message, args) {
-        message.reply({
-            content: "Pong!"
+
+        const heartbeat = this.client.ws.ping;
+        const response = Date.now() - message.createdTimestamp;
+        const total = heartbeat + response;
+
+        return message.reply({
+            embeds: [
+                {
+                    title: "🏓 Pong!",
+                    description: `⏱️ | Response: \`${response}ms\`\n❤️ | Heartbeat: \`${heartbeat}ms\`\n─────────────────\nTotal: \`${total}ms\``,
+                    color: 5814783
+                  }
+            ]
+        }).catch(() => {
+            return;
+        });
+    };
+
+    /**
+     * @param {CommandInteraction} i 
+     */
+    interact (i) {
+
+        const heartbeat = this.client.ws.ping;
+        const response = Date.now() - i.createdTimestamp;
+        const total = heartbeat + response;
+
+        return i.reply({
+            embeds: [
+                {
+                    title: "🏓 Pong!",
+                    description: `⏱️ | Response: \`${response}ms\`\n❤️ | Heartbeat: \`${heartbeat}ms\`\n─────────────────\nTotal: \`${total}ms\``,
+                    color: 5814783
+                  }
+            ]
+        }).catch(() => {
+            return;
         });
     };
 };
