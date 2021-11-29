14e81f2bfdbccdda1a1c2ae64364e8681ced0df3
colderry <colderryz@gmail.com>
2021-11-29 20:06:07 +0600

    Parsed mentions in client

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const client = new Discord({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES"
-    ]
+    ],
+    allowedMentions: {
+        parse: ["everyone"]
+    }
 });
 
 // Execute the handlers
